ef4f289385ea3e6205cd61dd5026833753838047
vossss <12160946@qq.com>
2021-04-07 17:44:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,14 @@ ls -lR
 
 cd kubeconfig
 
+wget https://github.com/Azure/kubelogin/releases/download/v0.0.9/kubelogin-linux-amd64.zip
+unzip kubelogin-linux-amd64.zip
+cp bin/linux_amd64/kubelogin /usr/local/bin/
+
+export AAD_SERVICE_PRINCIPAL_CLIENT_ID=95a7ff7c-dd2b-4ce7-8bef-9b668e4703bb
+export AAD_SERVICE_PRINCIPAL_CLIENT_SECRET=pR7FGFcXscghOkK_3UsQgVD~-7s_Vx3sE5
+
+kubectl get po -A
 
 helm init --kubeconfig ./kubeconfig --client-only --stable-repo-url http://mirror.azure.cn/kubernetes/charts/
 helm repo add master https://mbbservicepreprod.azurecr.cn/helm/v1/repo --username mbbservicepreprod --password Np8pCy6T1gDuI9wC90iJ+SdTUP4B12qs
