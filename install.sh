359e8414b0f5927b2d2b0f21ecd75f06eaf1cc86
vossss <12160946@qq.com>
2021-04-07 15:18:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@
 #     HomePage: 
 # =====================================
 
-cd kubeconfig
 
 ls -lR
+
+cd kubeconfig
+
 
 helm init --kubeconfig ./kubeconfig --client-only --stable-repo-url http://mirror.azure.cn/kubernetes/charts/
 helm repo add master https://mbbservicepreprod.azurecr.cn/helm/v1/repo --username mbbservicepreprod --password Np8pCy6T1gDuI9wC90iJ+SdTUP4B12qs
